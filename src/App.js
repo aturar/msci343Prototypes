4e7c1373b3ed8456e3bd35d098b5665192fd0d2e
Ayat Turar <aturaru@gmail.com>
2018-11-22 02:33:34 -0500

    Settign up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// vendors && utils
+import React from 'react';
+import { compose } from "recompose"
+import { Route, Switch, BrowserRouter } from "react-router-dom"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// pages
+import HomePage from "./pages/HomePage"
+
+function App(props) {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={() => <HomePage {...props} />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App;
+export default compose()(App)
